349a74da34ef9cbf88e815d8318b8f9d2379351d
Vadim Kozlov <shikter@greeny.cs.tlu.ee>
2016-02-29 03:54:39 +0200

    added register form

---
+++ check.php
@@ -0,0 +1,32 @@
+﻿<?php
+
+require_once("../../../config.php");
+
+// Скрипт проверки
+
+# Соединямся с БД
+//$mysql = new mysqli
+$link=mysqli_connect("localhost", $db_username, $db_password, "webpr2016_shikter"));
+
+if (isset($_COOKIE['id']) and isset($_COOKIE['hash']))
+{
+    $query = mysqli_query($link, "SELECT *,INET_NTOA(user_ip) AS user_ip FROM users WHERE user_id = '".intval($_COOKIE['id'])."' LIMIT 1");
+    $userdata = mysqli_fetch_assoc($query);
+
+    if(($userdata['user_hash'] !== $_COOKIE['hash']) or ($userdata['user_id'] !== $_COOKIE['id'])
+ or (($userdata['user_ip'] !== $_SERVER['REMOTE_ADDR'])  and ($userdata['user_ip'] !== "0")))
+    {
+        setcookie("id", "", time() - 3600*24*30*12, "/");
+        setcookie("hash", "", time() - 3600*24*30*12, "/");
+        print "Хм, что-то не получилось";
+    }
+    else
+    {
+        print "Привет, ".$userdata['user_login'].". Всё работает!";
+    }
+}
+else
+{
+    print "Включите куки";
+}
+?>
---
+++ register.php
@@ -0,0 +1,91 @@
+﻿<?php
+
+require_once("../../../config.php");
+
+// Страница регистрации нового пользователя
+
+
+	/*
+
+		//1 servername
+		//2 username
+		//3 password
+		//4 database
+		$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_shikter");
+
+		$stmt = $mysql->prepare("INSERT INTO Registration(user_login, user_password, user_hash) VALUES(?,?,?)");
+		
+		echo $mysql->error;
+		
+		// we are replacing question marks with values
+		// s -string, date or smth that is based on characters and numbers.
+		// i - integer, number
+		// d - decimal, floatval
+		
+		// for each question mark its type with one letter
+		$stmt->bind_param("sss", $_GET["who"], $_GET["message"], $_GET["from_who"]);
+		
+		//save
+		if($stmt->execute()){
+			echo "saved sucessfully";
+		}else{
+			echo $stmt->error;
+		}
+	*/
+		
+		
+
+# Соединямся с БД
+//$mysql = new mysqli
+$link=mysqli_connect("localhost", $db_username, $db_password, "webpr2016_shikter"));
+
+if(isset($_POST['submit']))
+{
+    $err = array();
+
+    # проверям логин
+    if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST['login']))
+    {
+        $err[] = "Логин может состоять только из букв английского алфавита и цифр";
+    }
+
+    if(strlen($_POST['login']) < 3 or strlen($_POST['login']) > 30)
+    {
+        $err[] = "Логин должен быть не меньше 3-х символов и не больше 30";
+    }
+
+    # проверяем, не сущестует ли пользователя с таким именем
+    $query = mysqli_query($link, "SELECT COUNT(user_id) FROM users WHERE user_login='".mysqli_real_escape_string($link, $_POST['login'])."'");
+    if(mysqli_num_rows($query) > 0)
+    {
+        $err[] = "Пользователь с таким логином уже существует в базе данных";
+    }
+
+    # Если нет ошибок, то добавляем в БД нового пользователя
+    if(count($err) == 0)
+    {
+
+        $login = $_POST['login'];
+
+        # Убераем лишние пробелы и делаем двойное шифрование
+        $password = md5(md5(trim($_POST['password'])));
+
+        mysqli_query($link,"INSERT INTO users SET user_login='".$login."', user_password='".$password."'");
+        header("Location: login.php"); exit();
+    }
+    else
+    {
+        print "<b>При регистрации произошли следующие ошибки:</b><br>";
+        foreach($err AS $error)
+        {
+            print $error."<br>";
+        }
+    }
+}
+?>
+
+<form method="POST">
+Login <input name="login" type="text"><br>
+Password <input name="password" type="password"><br>
+<input name="submit" type="submit" value="Зарегистрироваться">
+</form>
